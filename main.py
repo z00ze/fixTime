30cf26c539e21e8cdd18449ef71839ad66813d00
Marko Loponen <mmmarko@live.com>
2020-11-06 08:56:04 +0200

    some minor fixes

--- main.py
+++ main.py
@@ -7,57 +7,71 @@ output_folder = os.path.join("output/")
 
 
 
-def fixTime(input_folder, file, col, row, end_row, debug):
+def fixTime(input_folder, output_folder, name, time_col, header_col, end_row, debug):
     """ Reads excel file and fixes the time in a column
     """
-    wb = xl.load_workbook(filename = input_folder + file, data_only=True)
-    if debug: print("Fixing : " + input_folder + file)
+    # Start row
+    row = 5
+    wb = xl.load_workbook(filename = input_folder + name, data_only=True)
+    if debug: print("Fixing : " + input_folder + name)
     for sheet in wb:
         while row < end_row:
-            if len(str(sheet[col+str(row)].value)) == 0:
+
+            if len(str(sheet[header_col+str(row)].value)) == 0 or str(sheet[header_col+str(row)].value) == "None":
                 row += 1
                 continue
-            if len(str(sheet[col+str(row)].value).split(" ")) > 1:
-                sheet[col+str(row)].value = str(sheet[col+str(row)].value).split(" ")[1]
-            datas = str(sheet[col+str(row)].value).split(':')
-            
-            if len(datas) == 3:
-                
-                sheet[col+str(row)] = "00:%s:%s,000" % (datas[0].zfill(2), datas[1].zfill(2))
-                
-                if int(datas[1]) + 3 > 59:
-                    datas[0] = str(int(datas[1]) + 1)
-                    datas[1] = str((int(datas[1]) + 3) % 60)
-                else:
-                    datas[1] = str(int(datas[1])+3)
+            else:
+                if len(str(sheet[time_col+str(row)].value)) == 0 or str(sheet[time_col+str(row)].value) == "None":
+                    header = str(sheet[header_col+str(row)].value).replace(" ", "_").replace(".", "_").replace("?", "_").replace(":", "_")
+                    print(header)
+                    file = open(output_folder+header+".srt","w")
+                    index = 1
+                    row += 2
                     
-                sheet[col+str(row+1)] = "00:%s:%s,000" % (datas[0].zfill(2), datas[1].zfill(2))
-            row += 2
-            
-    """ Saving the modified xlsx to output folder
-    """
-    if debug: print("Saving : " + output_folder+file)
-    wb.save(output_folder+file)
+                    while sheet[time_col+str(row)].value != None:
+                        if len(str(sheet[time_col+str(row)].value).split(" ")) > 1:
+                            sheet[time_col+str(row)].value = str(sheet[time_col+str(row)].value).split(" ")[1]
+                        datas = str(sheet[time_col+str(row)].value).split(':')
+                                 
+                        if len(datas) == 3:
+                            start_time = "00:%s:%s,000" % (datas[0].zfill(2), datas[1].zfill(2))
+                            if int(datas[1]) + 3 > 59:
+                                datas[0] = str(int(datas[0]) + 1)
+                                datas[1] = str((int(datas[1]) + 3) % 60)
+                            else:
+                                datas[1] = str(int(datas[1])+3)
+
+                            end_time = "00:%s:%s,000" % (datas[0].zfill(2), datas[1].zfill(2))
+                            file.write(str(index)+"\n")
+                            file.write(start_time + " --> " + end_time +"\n")
+                            file.write(str(sheet[header_col+str(row)].value)+"\n\n")
+                            index += 1
+                        row += 2
+                    file.close()
+
+        
 
 if __name__ == "__main__":
     """ Check if file exists in input folder and does not exist in output folder,
         then converts it to with fixTime function and save it to output folder.
     """
     # Start column
-    col = 'B'
-    # Start row
-    row = 7
+    time_col = 'B'
+    # Header column
+    header_col = 'C'
+    
     # End row
     end_row = 1000
-    while (True):
-    
-        if(len(os.listdir(input_folder)) > 0):
-            
-            inputs = os.listdir(input_folder)
-            outputs = os.listdir(output_folder)
-            
-            for i in inputs:
-                if not i in outputs:
-                    fixTime(input_folder, i, col, row, end_row, True)
 
-        time.sleep(1)
+    
+    if(len(os.listdir(input_folder)) > 0):
+
+        inputs = os.listdir(input_folder)
+
+        for i in inputs:
+
+            if i.split(".")[-1:][0] == "xlsx":
+                
+                fixTime(input_folder, output_folder, i, time_col, header_col, end_row, True)
+
+
